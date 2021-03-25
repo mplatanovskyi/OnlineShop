27c75b4727e2368d3e7a910d11ac29d319708407
Mikroas <llachesis.mutas@gmail.com>
2021-03-25 15:21:33 +0200

    Created tables Books and Customers

---
+++ OnlineShop/Books.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(20) NOT NULL, 
+    [Author] NCHAR(20) NOT NULL, 
+    [EditionYear] INT NOT NULL, 
+    [Count] INT NOT NULL, 
+    [Price] MONEY NOT NULL
+)
--- OnlineShop/Customers.sql
+++ OnlineShop/Customers.sql
@@ -1,7 +1,8 @@
-﻿CREATE TABLE [dbo].[Customer]
+﻿CREATE TABLE [dbo].[Customers]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [FirstName] NCHAR(20) NOT NULL, 
     [SecondName] NCHAR(20) NOT NULL, 
-    [Address] NCHAR(40) NOT NULL
+    [Address] NCHAR(40) NOT NULL, 
+    [Email] NCHAR(20) NULL
 )
