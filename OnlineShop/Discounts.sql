b80ceaacdf379c3509ed033d64bb46be9aad6a11
Mikroas <llachesis.mutas@gmail.com>
2021-03-25 18:05:29 +0200

    Added new tables Discounts and Orders

---
+++ OnlineShop/Discounts.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Discounts]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [BookId] INT NOT NULL FOREIGN KEY REFERENCES [Books](Id), 
+    [DiscountSize] DECIMAL(5, 2) NOT NULL CHECK (00.00 <= DiscountSize AND DiscountSize <= 100.00), 
+    [Message] TEXT NOT NULL, 
+    [DateStart] DATE NOT NULL CHECK(DateStart < DateEnd), 
+    [DateEnd] DATE NOT NULL, 
+    
+)
