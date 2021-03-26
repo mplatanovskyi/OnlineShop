980aa20d4831731444d0c452646b86874289ad6a
mplatanovskyi <nnovak82@gmail.com>
2021-03-26 19:25:01 +0200

    Update Warehouse.sql
    Added LastUpdated field, renamed Books to Book

--- OnlineShop/Warehouse.sql
+++ OnlineShop/Warehouse.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Warehouse]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [BookId] INT NULL FOREIGN KEY REFERENCES [Books] (Id), 
-    [Count] NCHAR(10) NULL
+    	[BookId] INT NULL FOREIGN KEY REFERENCES [Book] (Id), 
+    	[Count] NCHAR(10) NULL,
+    	[LastUpdated] DATETIME NULL
 )
