911d6a58747f9e30c2b65fc529ff8070a7ce97bb
mplatanovskyi <nnovak82@gmail.com>
2021-03-26 19:19:43 +0200

    Update and rename Orders.sql to Order.sql
    singular is much more preferable for table naming

---
+++ OnlineShop/Order.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+    [Id] INT NOT NULL PRIMARY KEY, 
+    [BookId] INT NOT NULL FOREIGN KEY REFERENCES [Book](Id), 
+    [DeliveryAddress] NCHAR(40) NOT NULL, 
+    [Count] INT NOT NULL, 
+    [CustomerId] INT NOT NULL FOREIGN KEY REFERENCES [Customer](Id), 
+)
--- OnlineShop/Orders.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Orders]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [BookId] INT NOT NULL FOREIGN KEY REFERENCES [Books](Id), 
-    [DestinationAdress] NCHAR(40) NOT NULL, 
-    [Count] INT NOT NULL, 
-    [CustomerId] INT NOT NULL FOREIGN KEY REFERENCES [Customers](Id), 
-)
